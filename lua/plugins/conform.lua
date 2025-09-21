7d9bdf9f6b1b18fa3f5227f134c36eeb46299909
William Salazar <srwilliamg@gmail.com>
2025-09-21 13:01:48 -0500

    feat: add neotree and format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,5 +1,6 @@
 return {
   "stevearc/conform.nvim",
+  enabled = not vim.g.vscode,
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,26 @@
 return {
-"nvim-treesitter/nvim-treesitter",
- branch = 'master', lazy = false, build = ":TSUpdate",
- enabled = not vim.g.vscode,
-    opts ={
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "bash", "html", "javascript", "json", "lua", "go", "gomod",
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  enabled = not vim.g.vscode,
+  opts = {
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = {
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "go",
+      "gomod",
       "markdown",
       "markdown_inline",
       "python",
@@ -14,33 +30,25 @@ return {
       "typescript",
       "vim",
       "yaml",
-},
+    },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = true,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = true,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-  highlight = {
-    enable = true,
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    highlight = {
+      enable = true,
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
+    },
   },
-  }
 }

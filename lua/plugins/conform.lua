023447d6cd17b05512098614e630d1c7763f70fc
William Salazar <srwilliamg@gmail.com>
2025-09-21 12:51:43 -0500

    feat: add conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      go = { "gofmt" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      json = { "prettierd", "prettier", stop_after_first = true },
+      jsonc = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
+      jsx = { "prettierd", "prettier", stop_after_first = true },
+      yaml = { "prettier" }, -- or "yamlfmt"
+    },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+}

53c3631a377c169b374d3a15122bae16c31a5c55
William Salazar <srwilliamg@gmail.com>
2025-10-25 22:03:56 -0500

    feat: update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  enabled = not vim.g.vscode,
+  -- enabled = not vim.g.vscode,
+  enabled = true,
   branch = "master",
   lazy = false,
   build = ":TSUpdate",
   opts = {
+    indent = { enable = true }, ---@type lazyvim.TSFeat
+    highlight = { enable = true }, ---@type lazyvim.TSFeat
+    folds = { enable = true }, ---@type lazyvim.TSFeat
     -- A list of parser names, or "all" (the listed parsers MUST always be installed)
     ensure_installed = {
       "c",
@@ -20,6 +24,7 @@ return {
       "json",
       "lua",
       "go",
+      "gopls",
       "gomod",
       "markdown",
       "markdown_inline",
@@ -31,24 +36,7 @@ return {
       "vim",
       "yaml",
     },
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = true,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
-
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-    highlight = {
-      enable = true,
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
-    },
   },
 }

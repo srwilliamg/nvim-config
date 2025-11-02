a6f0e6d3561f89da6467bb695198d67650ee93d0
William Salazar <srwilliamg@gmail.com>
2025-11-02 17:44:10 -0500

    feat: update theme

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,10 +1,16 @@
 return {
   {
     enabled = not vim.g.vscode,
+    -- enabled = false,
     "ellisonleao/gruvbox.nvim",
     name = "gruvbox",
-    config = function()
-	vim.cmd.colorscheme("gruvbox")
+    init = function()
+      vim.cmd.colorscheme("gruvbox")
+      vim.o.background = "dark" -- or "light" for light mode
+      vim.cmd([[colorscheme gruvbox]])
+    end,
+    config = function(_, opts)
+      require("gruvbox").setup(opts)
     end,
     opts = {
       terminal_colors = true, -- add neovim terminal colors
@@ -26,9 +32,45 @@ return {
       inverse = true, -- invert background for search, diffs, statuslines and errors
       contrast = "", -- can be "hard", "soft" or empty string
       palette_overrides = {},
-      overrides = {},
+      overrides = {
+        -- dark background
+        CursorLineNr = { fg = "#fabd2f", bold = true },
+        Search = { bg = "#fabd2f", fg = "#1d2021" },
+        IncSearch = { bg = "#fabd2f", fg = "#1d2021" },
+        Visual = { bg = "#3c3836" },
+      },
       dim_inactive = false,
       transparent_mode = false,
     },
-  }
+  },
+  {
+    -- enabled = not vim.g.vscode,
+    enabled = false,
+    "rebelot/kanagawa.nvim",
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
+    end,
+    init = function()
+      require("kanagawa").load("dragon")
+      -- vim.cmd.colorscheme("kanagawa-dragon")
+    end,
+  },
+  {
+    enabled = false,
+    -- enabled = not vim.g.vscode,
+    "thesimonho/kanagawa-paper.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      theme = "kanagawa-paper-ink", -- The theme variant to use. Options are "kanagawa-paper-ink" and "kanagawa-paper-mist".
+      transparent = false, -- Enable/disable setting a transparent background.
+      terminalColors = true, -- Enable/disable setting terminal colors (vim.g.terminal_color_*) used in the terminal.
+    },
+    config = function(_, opts)
+      require("kanagawa-paper").setup(opts)
+    end,
+    init = function()
+      vim.cmd.colorscheme("kanagawa-paper-ink")
+    end,
+  },
 }

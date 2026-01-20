781f829a3c8f1fe6959973fa79f61f194a0d77f8
William Salazar <srwilliamg@gmail.com>
2026-01-19 20:55:00 -0500

    feat: restore luasnip and remove mini snippets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,93 +1,105 @@
 return {
-  "saghen/blink.cmp",
-  version = "1.*",
-  enabled = not vim.g.vscode,
-  build = "cargo build --release",
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    "nvim-tree/nvim-web-devicons",
-    "echasnovski/mini.snippets",
-    -- "giuxtaposition/blink-cmp-copilot",
+
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
   },
-  opts = {
-    -- snippets = { preset = "luasnip" },
-    snippets = { preset = "mini_snippets" },
-    keymap = {
-      preset = "none",
-      ["<C-j>"] = { "select_next", "fallback" },
-      ["<C-k>"] = { "select_prev", "fallback" },
-      ["<CR>"] = { "select_and_accept" },
-      -- ["<C-space>"] = {
-      --   function(cmp)
-      --     cmp.show({ providers = { "snippets" } })
-      --   end,
-      -- },
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    enabled = not vim.g.vscode,
+    build = "cargo build --release",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "nvim-tree/nvim-web-devicons",
+      "L3MON4D3/LuaSnip",
+      -- "giuxtaposition/blink-cmp-copilot",
     },
-    completion = {
-      trigger = {
-        show_on_insert = true,
-      },
-      documentation = {
-        auto_show = true,
-      },
-      ghost_text = {
-        enabled = true,
-        show_with_menu = false,
-      },
-      menu = {
-        auto_show = true,
-        draw = {
-          treesitter = { "lsp" },
-          columns = {
-            { "kind_icon" },
-            { "label" },
-            { "source_name" },
-          },
+    opts = {
+      snippets = { preset = "luasnip" },
+      keymap = {
+        preset = "none",
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<CR>"] = { "select_and_accept" },
+        ["<C-space>"] = {
+          function(cmp)
+            cmp.show({ providers = { "snippets" } })
+          end,
         },
       },
-    },
-    sources = {
-      default = { "lazydev", "lsp", "snippets", "path", "buffer", "cmdline" },
-      providers = {
-        lsp = {
-          name = "LSP",
-          module = "blink.cmp.sources.lsp",
+      completion = {
+        trigger = {
+          show_on_insert = true,
+        },
+        documentation = {
+          auto_show = true,
+        },
+        ghost_text = {
           enabled = true,
-          async = true,
-          timeout_ms = 100, -- Allow Amazon Q suggestions more time to arrive.
+          show_with_menu = false,
         },
-        lazydev = {
-          name = "LazyDev",
-          module = "lazydev.integrations.blink",
-          score_offset = 100,
-        },
-        buffer = { max_items = 5 },
-        snippets = {
-          opts = {
-            friendly_snippets = true, -- default
-            extended_filetypes = {
-              markdown = { "jekyll" },
-              sh = { "shelldoc" },
-              php = { "phpdoc" },
-              cpp = { "unreal" },
-              go = { "go" },
+        menu = {
+          auto_show = true,
+          draw = {
+            treesitter = { "lsp" },
+            columns = {
+              { "kind_icon" },
+              { "label" },
+              { "source_name" },
             },
           },
         },
-        -- copilot = {
-        --   enabled = false,
-        --   name = "copilot",
-        --   module = "blink-cmp-copilot",
-        --   score_offset = 100,
-        --   async = true,
-        --   transform_items = function(_, items)
-        --     for _, item in ipairs(items) do
-        --       item.kind_icon = ""
-        --       item.kind_name = "Copilot"
-        --     end
-        --     return items
-        --   end,
-        -- },
+      },
+      sources = {
+        default = { "lazydev", "lsp", "snippets", "path", "buffer", "cmdline" },
+        providers = {
+          lsp = {
+            name = "LSP",
+            module = "blink.cmp.sources.lsp",
+            enabled = true,
+            async = true,
+            timeout_ms = 100, -- Allow Amazon Q suggestions more time to arrive.
+          },
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+          buffer = { max_items = 5 },
+          snippets = {
+            opts = {
+              friendly_snippets = true, -- default
+              extended_filetypes = {
+                markdown = { "jekyll" },
+                sh = { "shelldoc" },
+                php = { "phpdoc" },
+                cpp = { "unreal" },
+                go = { "go" },
+              },
+            },
+          },
+          -- copilot = {
+          --   enabled = false,
+          --   name = "copilot",
+          --   module = "blink-cmp-copilot",
+          --   score_offset = 100,
+          --   async = true,
+          --   transform_items = function(_, items)
+          --     for _, item in ipairs(items) do
+          --       item.kind_icon = ""
+          --       item.kind_name = "Copilot"
+          --     end
+          --     return items
+          --   end,
+          -- },
+        },
       },
     },
   },
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -30,20 +30,4 @@ return {
       },
     },
   },
-  {
-    "echasnovski/mini.snippets",
-    config = function()
-      local gen_loader = require("mini.snippets").gen_loader
-      require("mini.snippets").setup({
-        snippets = {
-          -- Load custom file with global snippets first (adjust for Windows)
-          gen_loader.from_file("~/.config/nvim/snippets/global.json"),
-
-          -- Load snippets based on current language by reading files from
-          -- "snippets/" subdirectories from 'runtimepath' directories.
-          gen_loader.from_lang(),
-        },
-      })
-    end,
-  },
 }

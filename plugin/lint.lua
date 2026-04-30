6e94931d4b7237942e8452b2076264039c04927b
William Salazar <srwilliamg@gmail.com>
2026-04-29 20:49:38 -0500

    chore: update

--- plugin/lint.lua
+++ plugin/lint.lua
@@ -1,23 +1,23 @@
 require("lazyload").on_vim_enter(function()
   vim.pack.add({
-    { src = Utils.github("mfussenegger/nvim-lint"), name = "lint" },
+    Utils.github("mfussenegger/nvim-lint"),
   })
 
   local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
   vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     group = lint_augroup,
     callback = function()
-      if vim.bo.modifiable then
-        require("lint").try_lint(nil, { ignore_errors = true })
-      end
+      -- if vim.bo.modifiable then
+      --   require("lint").try_lint(nil, { ignore_errors = true })
+      -- end
 
-      -- require("lint").linters_by_ft = {
-      --   markdown = { "vale" },
-      --   go = { "golangcilint" },
-      -- }
-      -- -- try_lint without arguments runs the linters defined in `linters_by_ft`
-      -- -- for the current filetype
-      -- require("lint").try_lint()
+      require("lint").linters_by_ft = {
+        markdown = { "vale" },
+        go = { "golangcilint" },
+      }
+      -- try_lint without arguments runs the linters defined in `linters_by_ft`
+      -- for the current filetype
+      require("lint").try_lint()
     end,
   })
 end)

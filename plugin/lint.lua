a56f3a837e898f6ce25d8ed786839ec41c74fb53
William Salazar <srwilliamg@gmail.com>
2026-05-03 09:31:55 -0500

    fix: lint

--- plugin/lint.lua
+++ plugin/lint.lua
@@ -6,17 +6,12 @@ require("lazyload").on_vim_enter(function()
   local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
   vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     group = lint_augroup,
+    -- pattern = { "*.lua", "*.json", "*.go", "*.js", "*.ts" },
     callback = function()
-      -- if vim.bo.modifiable then
-      --   require("lint").try_lint(nil, { ignore_errors = true })
-      -- end
-
       require("lint").linters_by_ft = {
         markdown = { "vale" },
         go = { "golangcilint" },
       }
-      -- try_lint without arguments runs the linters defined in `linters_by_ft`
-      -- for the current filetype
       require("lint").try_lint()
     end,
   })

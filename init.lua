5667093fde1f82602f63904b23b32cf5f3521b5d
William Salazar <srwilliamg@gmail.com>
2026-05-08 11:33:36 -0500

    feat: general improvements 8

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- vim.opts
+vim.opt.clipboard = ""
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 vim.opt.nu = true
 vim.opt.relativenumber = true
